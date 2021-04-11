4f8516ad1babfe1c8f1d6e8638a121174f2319ba
rainbowtalaxy <fujianchensiwei@163.com>
2021-04-11 22:56:55 +0800

    redesign markdown framework

--- Sources/Markdown/Rules.swift
+++ Sources/Markdown/Rules.swift
@@ -7,72 +7,68 @@
 
 import Foundation
 
-extension Markdown {
+open class Element: Identifiable {
+    public let id: UUID
     
-    open class Element: Identifiable {
-        public let id: UUID
-        
-        public init(raw: Raw, resolver: Resolver? = nil) {
-            self.id = UUID()
-        }
+    public init(raw: Raw, resolver: Resolver? = nil) {
+        self.id = UUID()
+    }
+}
+
+
+
+open class SplitRule {
+    public let priority: Double
+    
+    public init(priority: Double) {
+        self.priority = priority
     }
     
+    open func split(from text: String) -> [Raw] {
+        return [Raw(lock: false, text: text)]
+    }
     
-    
-    open class SplitRule {
-        public let priority: Double
-        
-        public init(priority: Double) {
-            self.priority = priority
-        }
-        
-        open func split(from text: String) -> [Raw] {
-            return [Raw(lock: false, text: text)]
-        }
-        
-        final func splitAll(raws: [Raw]) -> [Raw] {
-            var result: [Raw] = []
-            for raw in raws {
-                if raw.lock {
-                    result.append(raw)
-                } else {
-                    result.append(contentsOf: self.split(from: raw.text))
-                }
+    final func splitAll(raws: [Raw]) -> [Raw] {
+        var result: [Raw] = []
+        for raw in raws {
+            if raw.lock {
+                result.append(raw)
+            } else {
+                result.append(contentsOf: self.split(from: raw.text))
             }
-            return result
         }
-        
-        public final func split(by regex: String, text: String, type: String) -> [Raw] {
-            var elements: [Raw] = []
-            let splitResult = text.split(by: regex, options: lineRegexOption)
-            for section in splitResult.result {
-                let content = String(splitResult.raw[section.range]).trimLine()
-                if section.match {
-                    elements.append(Raw(lock: true, text: content, type: type))
-                } else {
-                    elements.append(Raw(lock: false, text: content, type: nil))
-                }
+        return result
+    }
+    
+    public final func split(by regex: String, text: String, type: String) -> [Raw] {
+        var elements: [Raw] = []
+        let splitResult = text.split(by: regex, options: lineRegexOption)
+        for section in splitResult.result {
+            let content = String(splitResult.raw[section.range]).trimLine()
+            if section.match {
+                elements.append(Raw(lock: true, text: content, type: type))
+            } else {
+                elements.append(Raw(lock: false, text: content, type: nil))
             }
-            return elements
         }
+        return elements
+    }
+}
+
+
+
+open class MapRule {
+    public let priority: Double
+    
+    public init(priority: Double) {
+        self.priority = priority
     }
     
-    
-    
-    open class MapRule {
-        public let priority: Double
-        
-        public init(priority: Double) {
-            self.priority = priority
-        }
-        
-        open func map(from raw: Raw, resolver: Resolver?) -> Element? {
-            return nil
-        }
-        
-        public final func map(from raw: Raw) -> Element? {
-            return map(from: raw, resolver: nil)
-        }
+    open func map(from raw: Raw, resolver: Resolver?) -> Element? {
+        return nil
     }
     
+    public final func map(from raw: Raw) -> Element? {
+        return map(from: raw, resolver: nil)
+    }
 }
--- Sources/Markdown/statics.swift
+++ Sources/Markdown/statics.swift
@@ -7,39 +7,36 @@
 
 import Foundation
 
-extension Markdown {
+public let lineRegexOption: NSRegularExpression.Options = [.anchorsMatchLines]
+
+public struct TypeMap {
     
-    public static let lineRegexOption: NSRegularExpression.Options = [.anchorsMatchLines]
+    public static let header = "header"
     
-    public struct TypeMap {
-        
-        public static let header = "header"
-        
-        public static let quote = "quote"
-        
-        public static let code = "code"
-        
-        public static let unorderlist = "ul"
-        
-        public static let orderlist = "ol"
-        
-        public static let line = "line"
-        
-    }
+    public static let quote = "quote"
     
-    public static let defaultSplitRules: [SplitRule] = [
+    public static let code = "code"
+    
+    public static let unorderlist = "ul"
+    
+    public static let orderlist = "ol"
+    
+    public static let line = "line"
+    
+}
+
+public let defaultSplitRules: [SplitRule] = [
 //        ListSplitRule(priority: 0),
 //        CodeSplitRule(priority: 1),
-        HeaderSplitRule(priority: 2),
+    HeaderSplitRule(priority: 2),
 //        QuoteSplitRule(priority: 3),
 //        LineSplitRule(priority: 4)
-    ]
+]
 
-    public static let defaultMapRules: [MapRule] = [
-        HeaderMapRule(priority: 0),
+public let defaultMapRules: [MapRule] = [
+    HeaderMapRule(priority: 0),
 //        QuoteMapRule(priority: 1),
 //        CodeMapRule(priority: 2),
 //        ListMapRule(priority: 3),
 //        LineMapRule(priority: 4)
-    ]
-}
+]
